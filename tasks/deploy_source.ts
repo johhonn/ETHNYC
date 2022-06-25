0c3a82eb228cde20eb12e88606fd4a286559f50c
base0010 <marcus@gatekeeper.network>
2022-06-25 14:21:58 -0600

    hre added

--- tasks/deploy_source.ts
+++ tasks/deploy_source.ts
@@ -2,8 +2,6 @@ import { IncrementalMerkleTree } from "@zk-kit/incremental-merkle-tree"
 import { poseidon } from "circomlibjs"
 import { Contract } from "ethers"
 import { task, types } from "hardhat/config"
-import hre from "hardhat"
-import identityCommitments from "../public/identityCommitments.json"
 
 
 const RinkebyConnextHandler = "0x2307Ed9f152FA9b3DcDfe2385d279D8C2A9DF2b0";
@@ -11,7 +9,7 @@ const GoerliConenxtHandler = "0xEC3A723DE47a644b901DC269829bf8718F175EBF"
 
 task("deploy-source", "")
     .addOptionalParam<boolean>("logs", "Print the logs", true, types.boolean)
-    .setAction(async ({ logs }, { ethers }): Promise<Contract | void> => {
+    .setAction(async ({ logs }, { ethers, network }): Promise<Contract | void> => {
         const SourceContract = await ethers.getContractFactory("Source");
         const Source = await SourceContract.deploy()
 
@@ -21,7 +19,7 @@ task("deploy-source", "")
 
         const SourceContractInstance = await ethers.getContractFactory("Source")
 
-        const chainID = hre.network.config.chainId;
+        const chainID = network.config.chainId;
         let source;
         if ( chainID === 4) {
             source = await SourceContractInstance.deploy(RinkebyConnextHandler);
