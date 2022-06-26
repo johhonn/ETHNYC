d7d1d3611e1b87eb33b70b977e1a6de58bdec599
base0010 <marcus@gatekeeper.network>
2022-06-26 01:44:45 -0600

    addcommitment task w/o values

--- tasks/addCommitment.ts
+++ tasks/addCommitment.ts
@@ -1,27 +1,21 @@
 import { Contract } from "ethers";
+import { id } from "ethers/lib/utils";
 import { task, types } from "hardhat/config";
 
-const RinkebyConnextHandler = "0x2307Ed9f152FA9b3DcDfe2385d279D8C2A9DF2b0";
 const KovanConnextHandler = "0x3366A61A701FA84A86448225471Ec53c5c4ad49f";
 
-task("deploy_source", "")
+task("addCommitment", "")
   .addOptionalParam<boolean>("logs", "Print the logs", true, types.boolean)
   .setAction(async ({ logs }, hre): Promise<Contract | void> => {
     const SourceContractInstance = await hre.ethers.getContractFactory(
       "Source"
     );
 
-    const chainID = hre.network.config.chainId;
-    console.log(hre.network.name);
-    console.log(chainID);
-    let source;
-    if (hre.network.name === "rinkeby") {
-      source = await SourceContractInstance.deploy(RinkebyConnextHandler);
-    } else if (hre.network.name === "kovan") {
-      source = await SourceContractInstance.deploy(KovanConnextHandler);
+    const source = await SourceContractInstance.attach(KovanConnextHandler);
+    const isDeployed = await source?.deployed();
+    if (isDeployed) {
+      source.addCommitment();
     }
-
-    await source?.deployed();
 
     logs &&
       console.log(`Source contract has been deployed to: ${source?.address}`);
